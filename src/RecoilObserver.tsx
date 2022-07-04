ab457af01993edbd912437c8177044e3362dabf1
dudlf <dudlf016@gmail.com>
2022-07-04 21:38:40 +0900

    feat: create new context

--- src/RecoilObserver.tsx
+++ src/RecoilObserver.tsx
@@ -6,109 +6,120 @@ export type AtomListener<T> = (value: T) => void
 // vscode syntax highlighting not working when this is inlined
 type AtomListenerSetOrNull = Set<AtomListener<any> | null>
 
-const versionAtom = atom({
-  key: '__recoil_observer_version',
-  default: 0,
-})
-
-// this trigger will update recoil-observer up-to date
-let trigger: (() => void) | undefined = undefined
-
-const atomObservers = new Map<RecoilState<any>, JSX.Element>;
-const atomListeners = new Map<RecoilState<any>, AtomListenerSetOrNull>;
-
-let garbages: RecoilState<any>[] = []
-
-function clearGarbages() {
-  garbages.forEach((atom) => {
-    if (atomListeners.get(atom)?.size === 0) {
-      atomObservers.delete(atom)
-    }
+export function createContext() {
+  const versionAtom = atom({
+    key: '__recoil_observer_version',
+    default: 0,
   })
-  garbages = []
-}
 
-/**
- *  Root jsx element of recoil-observer
- */
-export function RecoilObserver() {
-  // subscribe version to update atom observers
-  const [_, setVersion] = useRecoilState(versionAtom)
+  // this trigger will update recoil-observer up-to date
+  let trigger: (() => void) | undefined = undefined
 
-  useEffect(() => {
-    if (garbages.length > 0) {
-      clearGarbages()
-    }
+  const atomObservers = new Map<RecoilState<any>, JSX.Element>()
+  const atomListeners = new Map<RecoilState<any>, AtomListenerSetOrNull>()
 
-    trigger = () => {
-      setVersion((curr) => curr + 1)
-    }
+  let garbages: RecoilState<any>[] = []
 
-    return () => {
-      trigger = undefined
-    }
-  }, [])
-
-  const observers = Array.from(atomObservers.values())
-
-  return <>{observers}</>
-}
-
-/**
- *  JSX Element which listen change on given atom
- */
-function AtomObserver({atom}: {atom: RecoilState<any>}) {
-  const value = useRecoilValue(atom)
-
-  useEffect(() => {
-    atomListeners.get(atom)?.forEach((fn) => {
-      fn?.(value)
+  function clearGarbages() {
+    garbages.forEach((atom) => {
+      if (atomListeners.get(atom)?.size === 0) {
+        atomObservers.delete(atom)
+      }
     })
-  }, [value])
-
-  return <></>
-}
-
-/**
- *  subscribe `fn` for listening `atom`
- */
-export function subscribe<T>(atom: RecoilState<T>, fn: AtomListener<T> | null) {
-  const listenerSet = atomListeners.get(atom) || new Set()
-
-  if (fn) {
-    atomListeners.set(atom, listenerSet)
-    listenerSet.add(fn)
+    garbages = []
   }
-  
-  const observer = atomObservers.get(atom) || <AtomObserver atom={atom} key={atom.key}/>
-  atomObservers.set(atom, observer)
 
-  // update recoil-observer root
-  trigger?.()
-}
+  /**
+   *  Root jsx element of recoil-observer
+   */
+  function RecoilObserver() {
+    // subscribe version to update atom observers
+    const [_, setVersion] = useRecoilState(versionAtom)
 
-/**
- *  unsubscribe `fn` from listeners for given `atom`
- * 
- *  if there is no listner listening `atom`, the observer jsx element will be removed immediately
- */
-export function unsubscribe<T>(atom: RecoilState<T>, fn: AtomListener<T>) {
-  const listenerSet = atomListeners.get(atom)
+    useEffect(() => {
+      if (garbages.length > 0) {
+        clearGarbages()
+      }
 
-  if (listenerSet) {
-    listenerSet.delete(fn)
+      trigger = () => {
+        setVersion((curr) => curr + 1)
+      }
 
-    if (listenerSet.size === 0) {
+      return () => {
+        trigger = undefined
+      }
+    }, [])
+
+    const observers = Array.from(atomObservers.values())
+
+    return <>{observers}</>
+  }
+
+  /**
+   *  JSX Element which listen change on given atom
+   */
+  function AtomObserver({ atom }: { atom: RecoilState<any> }) {
+    const value = useRecoilValue(atom)
+
+    useEffect(() => {
+      atomListeners.get(atom)?.forEach((fn) => {
+        fn?.(value)
+      })
+    }, [value])
+
+    return <></>
+  }
+
+  /**
+   *  subscribe `fn` for listening `atom`
+   */
+  function subscribe<T>(atom: RecoilState<T>, fn: AtomListener<T> | null) {
+    const listenerSet = atomListeners.get(atom) || new Set()
+
+    if (fn) {
+      atomListeners.set(atom, listenerSet)
+      listenerSet.add(fn)
+    }
+
+    const observer = atomObservers.get(atom) || <AtomObserver atom={atom} key={atom.key} />
+    atomObservers.set(atom, observer)
+
+    // update recoil-observer root
+    trigger?.()
+  }
+
+  /**
+   *  unsubscribe `fn` from listeners for given `atom`
+   *
+   *  if there is no listner listening `atom`, the observer jsx element will be removed immediately
+   */
+  function unsubscribe<T>(atom: RecoilState<T>, fn: AtomListener<T>) {
+    const listenerSet = atomListeners.get(atom)
+
+    if (listenerSet) {
+      listenerSet.delete(fn)
+
+      if (listenerSet.size === 0) {
         atomObservers.delete(atom)
         // we can't call `trigger`
         garbages.push(atom)
+      }
     }
+  }
+
+  /**
+   *  return the number of subscribers of given `atom`
+   */
+  function size(atom: RecoilState<any>) {
+    return atomListeners.get(atom)?.size || 0
+  }
+
+  return {
+    subscribe,
+    unsubscribe,
+    size,
+    RecoilObserver,
   }
 }
 
-/**
- *  return the number of subscribers of given `atom`
- */
-export function size(atom: RecoilState<any>) {
-  return atomListeners.get(atom)?.size || 0
-}
+export const { subscribe, unsubscribe, size, RecoilObserver } = createContext()
